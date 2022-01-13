755bf1520443bf4ae544d2627f3753602bc4654f
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-01-13 17:36:56 -0500

    home page view added

--- index.php
+++ index.php
@@ -13,7 +13,9 @@ $f3 = Base::instance();
 //define a default route (echo hello world when going to default route - root directory of project)
 $f3->route('GET /', function()
 {
-    echo "<h1>Hello, World!</h1>"; //this is currently the view, but not a view page
+    //echo "<h1>Hello, World!</h1>"; //this is currently the view, but not a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //run fat-free! -> invokes
